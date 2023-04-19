38aeaa6b92408b29b6ca67fd77dd8e004a330abb
Adrian Navarro Gallardo <agry.code@gmail.com>
2023-04-19 19:39:58 +0200

    background y responsive
    responsive pero no responsive sentimental :alien:

--- wordle.js
+++ wordle.js
@@ -37,7 +37,7 @@ function intialize() {
     //Create the keyboard
     let keyboard=[
         ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
-        ["A", "S", "D", "F", "G", "H", "J", "K", "L", " "],
+        ["A", "S", "D", "F", "G", "H", "J", "K", "L", "Ñ"],
         ["Enter", "Z", "X", "C", "V", "B", "N", "M", "⌫" ]
     ]
 
